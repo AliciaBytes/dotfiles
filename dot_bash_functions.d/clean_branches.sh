99fa70824be86e0393add64aa11d3487b3e02b59
Alicia Miedl <alicia@aliciabytes.com>
2024-04-18 20:05:45 +0200

    Change branch cleaning behaviour

--- dot_bash_functions.d/clean_branches.sh
+++ dot_bash_functions.d/clean_branches.sh
@@ -1,6 +1,8 @@
 clean_branches() {
-    git fetch -p &&
-        git for-each-ref --format '%(refname:short) %(upstream:track)' |
+    # Don't fetch information about newly gone branches. Just clean up
+    # the ones we already knew are gone!
+    # git fetch -p &&
+    git for-each-ref --format '%(refname:short) %(upstream:track)' |
         awk '$2 == "[gone]" {print $1}' |
-            xargs -r git branch -D
+        xargs -r git branch -D
 }
