7da63c4b78886bdd63588860df2d3cc49f137b11
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2024-04-20 20:57:38 +0200

    Add a `git cleanup` command
    The `git cleanup` command will automatically delete merged and gone branches.

--- dot_bash_functions.d/clean_branches.sh
+++
@@ -1,8 +0,0 @@
-clean_branches() {
-    # Don't fetch information about newly gone branches. Just clean up
-    # the ones we already knew are gone!
-    # git fetch -p &&
-    git for-each-ref --format '%(refname:short) %(upstream:track)' |
-        awk '$2 == "[gone]" {print $1}' |
-        xargs -r git branch -D
-}
