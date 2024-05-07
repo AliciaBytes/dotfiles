f1e711a6cdd440ba10297b38cdb7ef0f5922bf44
Alicia Jasmine Miedl <alicia@aliciabytes.com>
2024-05-07 06:27:19 +0200

    Add fnm config for fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -2,4 +2,5 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     source (starship init fish --print-full-init | psub)
     atuin init fish | source
+    fnm env --use-on-cd | source
 end
